3d1bd9fded26bf37b5ad68b0cad8fedc05e8580f
Joaquim-SNeto <netojoaquimjj@gmail.com>
2026-04-15 16:35:23 -0300

    test: adiciona arquivo testeAtribuicao.c para validar atribuições

---
+++ tests/testeAtribuicao.c
@@ -0,0 +1,27 @@
+int a = 10;
+int b = 5;
+float x = 2.5;
+char c = 'A';
+
+a = 20;
+b = 15;
+
+a = 30;
+x = 3.14;
+
+a = b;
+x = a;
+
+a = b + 5;
+b = a - 3;
+x = x * 2.0;
+a = a + b;
+
+x = a + 2.5;
+a = x;        
+c = 'B';
+
+int d = 0;
+d = a + b + 10;
+a = d - b;
+x = d * 1.5;
